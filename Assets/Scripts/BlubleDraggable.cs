3dbeeac7e855ac32feecb96bce7676bdde6c3532
stuetz-lena <65137171+stuetz-lena@users.noreply.github.com>
2023-01-13 18:15:25 +0100

    Live adjustments 8

--- Assets/Scripts/BlubleDraggable.cs
+++ Assets/Scripts/BlubleDraggable.cs
@@ -122,6 +122,7 @@ public class BlubleDraggable : GrabbableBase<PointerEventData, BlubleDraggable.G
         if(!isHit){ //if the bubble was already sorted in, do not act again 
             Collider myCollider = gameObject.GetComponent<Collider>();
             if((other.collider.CompareTag("Bucket_der") && myCollider.CompareTag("der")) || (other.collider.CompareTag("Bucket_die") && myCollider.CompareTag("die")) || (other.collider.CompareTag("Bucket_das") && myCollider.CompareTag("das"))){ //correct sorting
+                 Debug.Log("here correct " + this.GetComponentInChildren<TextMeshPro>().text);
                 Debug.Log("Correct!");
                 isHit = true; //save sorting
 
@@ -213,10 +214,17 @@ public class BlubleDraggable : GrabbableBase<PointerEventData, BlubleDraggable.G
     }
 
     IEnumerator DestroyBluble(float time){ //MasterONLYfunction
+this.GetComponent<PhotonView>().TransferOwnership(0);
+
         yield return new WaitForSeconds(time); //wait for audio clip end
+
+
+
         if(this.GetComponent<PhotonView>().IsMine)
             PhotonNetwork.Destroy(this.gameObject);
     }
+
+
     //---- addedCodeEnd ------
 
      [Serializable]
